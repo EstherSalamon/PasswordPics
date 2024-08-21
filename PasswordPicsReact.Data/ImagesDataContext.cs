6c62abadcd0107caca37d6991bc01c96971414a0
Esther Salamon <esther386386@gmail.com>
2024-08-21 13:39:13 -0400

    Add project files.

---
+++ PasswordPicsReact.Data/ImagesDataContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace PasswordPicsReact.Data;
+
+public class ImagesDataContext : DbContext
+{
+    private readonly string _connectionString;
+
+    public ImagesDataContext(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_connectionString);
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+        {
+            relationship.DeleteBehavior = DeleteBehavior.Restrict;
+        }
+    }
+}
